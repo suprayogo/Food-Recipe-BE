b69b12f222a1b7df5fb595a91bf29c62c215dfda
suprayogo <126061696+suprayogo@users.noreply.github.com>
2023-07-25 06:43:38 +0700

    update controller recipes popular

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,5 +1,5 @@
-const db = require('../database')
-const model = require('../models/recipes.models')
+const db = require("../database");
+const model = require("../models/recipes.models");
 const cloudinary = require("cloudinary").v2;
 const jwt = require("jsonwebtoken");
 
@@ -15,89 +15,139 @@ function getToken(req) {
 const getRecipesById = async (req, res) => {
   try {
     const {
-      params: { id }
-    } = req
+      params: { id },
+    } = req;
 
     if (isNaN(id)) {
       res.status(400).json({
         status: false,
-        message: 'ID must be integer'
-      })
+        message: "ID must be integer",
+      });
 
-      return
+      return;
     }
 
-    const query = await model.setGetRecipesByid(id)
+    const query = await model.setGetRecipesByid(id);
 
     res.json({
       status: true,
-      message: 'Get data success',
-      data: query
-    })
+      message: "Get data success",
+      data: query,
+    });
   } catch (error) {
     res.status(500).json({
       status: false,
-      message: 'Error in server'
-    })
+      message: "Error in server",
+    });
   }
-}
+};
+
 
 const getAllRecipes = async (req, res) => {
   try {
-    let query
-    const keyword = `%${req?.query?.keyword}%`
-    let sort = db`DESC`
+    let query;
+    const keyword = `%${req?.query?.keyword}%`;
+    let sort = db`DESC`;
     const isPaginate =
       req?.query?.pages &&
       !isNaN(req?.query?.pages) &&
-      parseInt(req?.query?.pages) >= 1
+      parseInt(req?.query?.pages) >= 1;
+    const createdParam = req?.query?.created?.toLowerCase();
+    const sortColumn = req?.query?.sortColumn?.toLowerCase(); // Ambil nilai sortColumn dari permintaan
 
-    if (req?.query?.sortType?.toLowerCase() === 'asc') {
-      if (isPaginate) {
-        sort = db`ASC LIMIT 9 OFFSET ${
-          9 * (parseInt(req?.query?.pages) - 1)
-        }`
-      } else {
-        sort = db`ASC`
-      }
+    // Memeriksa jika sortColumn adalah "title", jika ya, kita gunakan untuk mengurutkan
+    if (sortColumn === "title") {
+      sort = db`ASC`; // Ubah urutan menjadi ASC jika ingin mengurutkan berdasarkan judul (title)
     }
 
     if (isPaginate && !req?.query?.sortType) {
-      sort = db`DESC LIMIT 9 OFFSET ${9 * (parseInt(req?.query?.pages) - 1)}`
+      sort = db`DESC LIMIT 9 OFFSET ${9 * (parseInt(req?.query?.pages) - 1)}`;
     }
 
     if (req?.query?.keyword) {
       query =
-        await db`SELECT *, count(*) OVER() AS full_count FROM recipes WHERE LOWER(recipes.title) LIKE LOWER(${keyword}) ORDER BY id ${sort}`
+        await db`SELECT *, count(*) OVER() AS full_count FROM recipes WHERE LOWER(recipes.title) LIKE LOWER(${keyword}) ORDER BY "createdAt" ${sort}`;
     } else {
       query =
-        await db`SELECT *, count(*) OVER() AS full_count FROM recipes ORDER BY id ${sort}`
+        await db`SELECT *, count(*) OVER() AS full_count FROM recipes ORDER BY "createdAt" ${sort}`;
     }
 
     res.json({
       status: !!query?.length,
-      message: query?.length ? 'Get data success' : 'Data not found',
+      message: query?.length ? "Get data success" : "Data not found",
       total: query?.length ?? 0,
       pages: isPaginate
         ? {
             current: parseInt(req?.query?.pages),
             total: query?.[0]?.full_count
               ? Math.ceil(parseInt(query?.[0]?.full_count) / 10)
-              : 0
+              : 0,
           }
         : null,
       data: query?.map((item) => {
-        delete item.full_count
-        return item
-      })
-    })
+        delete item.full_count;
+        return item;
+      }),
+    });
+  } catch (error) {
+    console.error("Error in getAllRecipes:", error);
+    res.status(500).json({
+      status: false,
+      message: "Error in server",
+      error: error.message,
+    });
+  }
+};
+
+
+const getRecipesPopular = async (req, res) => {
+  try {
+    let query;
+    const isPaginate =
+      req?.query?.pages &&
+      !isNaN(req?.query?.pages) &&
+      parseInt(req?.query?.pages) >= 1;
+
+    if (isPaginate) {
+      const offset = 9 * (parseInt(req?.query?.pages) - 1);
+      query = await db`
+        SELECT *, count(*) OVER() AS full_count
+        FROM recipes
+        ORDER BY COALESCE(likes, -1) DESC
+        LIMIT 9 OFFSET ${offset}
+      `;
+    } else {
+      query = await db`
+        SELECT *, count(*) OVER() AS full_count
+        FROM recipes
+        ORDER BY COALESCE(likes, -1) DESC
+      `;
+    }
+
+    res.json({
+      status: !!query?.length,
+      message: query?.length ? "Get data success" : "Data not found",
+      total: query?.length ?? 0,
+      pages: isPaginate
+        ? {
+            current: parseInt(req?.query?.pages),
+            total: query?.[0]?.full_count
+              ? Math.ceil(parseInt(query?.[0]?.full_count) / 10)
+              : 0,
+          }
+        : null,
+      data: query?.map((item) => {
+        delete item.full_count;
+        return item;
+      }),
+    });
   } catch (error) {
     res.status(500).json({
       status: false,
-      message: 'Error in server'
-    })
+      message: "Error in server",
+    });
   }
-}
+};
 
 const getProfileRecipes = async (req, res) => {
   try {
@@ -110,9 +160,11 @@ const getProfileRecipes = async (req, res) => {
         !isNaN(req?.query?.pages) &&
         parseInt(req?.query?.pages) >= 1;
 
-      if (req?.query?.sortType?.toLowerCase() === 'asc') {
+      if (req?.query?.sortType?.toLowerCase() === "asc") {
         if (isPaginate) {
-          sort = db`ASC LIMIT 9 OFFSET ${9 * (parseInt(req?.query?.pages) - 1)}`;
+          sort = db`ASC LIMIT 9 OFFSET ${
+            9 * (parseInt(req?.query?.pages) - 1)
+          }`;
         } else {
           sort = db`ASC`;
         }
@@ -140,7 +192,7 @@ const getProfileRecipes = async (req, res) => {
 
       res.json({
         status: !!query?.length,
-        message: query?.length ? 'Get data success' : 'Data not found',
+        message: query?.length ? "Get data success" : "Data not found",
         total: query?.length ?? 0,
         pages: isPaginate
           ? {
@@ -159,24 +211,54 @@ const getProfileRecipes = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       status: false,
-      message: 'Error in server',
+      message: "Error in server",
     });
   }
 };
 
-
 const addNewRecipes = async (req, res) => {
   try {
     const { title, ingredients, video_link } = req.body;
+    const { photo } = req?.files ?? {};
 
     // Validation
     if (!(title && ingredients && video_link)) {
       res.status(400).json({
         status: false,
-        message: 'Bad input, please complete all fields',
+        message: "Bad input, please complete all fields",
       });
       return;
     }
+
+    if (!photo) {
+      res.status(400).send({
+        status: false,
+        message: "Photo is required",
+      });
+      return;
+    }
+
+    let mimeType = photo.mimetype.split("/")[1];
+    let allowFile = ["jpeg", "jpg", "png", "webp"];
+
+    if (!allowFile?.find((item) => item === mimeType)) {
+      res.status(400).send({
+        status: false,
+        message: "Only accept jpeg, jpg, png, webp",
+      });
+      return;
+    }
+
+    // Validate size image
+    if (photo.size > 2000000) {
+      res.status(400).send({
+        status: false,
+        message: "File too big, max size 2MB",
+      });
+      return;
+    }
+
+    const recipePicture = await uploadImage(photo);
 
     // Retrieve the user ID from the token (assuming the ID is stored in req.userId)
     const created_by = req.userId;
@@ -184,117 +266,129 @@ const addNewRecipes = async (req, res) => {
     // Fetch the namechef from the users table based on the created_by ID
     const namechefResult = await db`SELECT fullname FROM users WHERE id = ${created_by}`;
 
-    // Construct the payload for the query, including the namechef
+    // Construct the payload for the query, including the namechef and the uploaded image URL
     const payload = {
       title,
       ingredients,
       video_link,
       created_by,
-      namechef: namechefResult[0]?.fullname || null, // Add the namechef to the payload
+      namechef: namechefResult[0]?.fullname || null,
+      recipePicture,
     };
 
     // Insert the recipe into the 'recipes' table using the db function (Assuming it is a query builder)
     const queryResult = await db`INSERT INTO recipes ${db(
       payload,
-      'title',
-      'ingredients',
-      'video_link',
-      'created_by',
-      'namechef' // Add the 'namechef' field to the db query
+      "title",
+      "ingredients",
+      "video_link",
+      "created_by",
+      "namechef",
+      "recipePicture" // Add the 'recipePicture' field to the db query
     )} returning *`;
 
     res.send({
       status: true,
-      message: 'Success insert data',
-      data: queryResult[0], // Since 'queryResult' is an array, access the first element
+      message: "Success insert data",
+      data: queryResult[0],
     });
   } catch (error) {
-    console.error(error); // Log the error for debugging purposes
-
-    res.status(500).json({
+    console.log(error);
+    res.status(500).send({
       status: false,
-      message: 'Internal Server Error',
+      message: "Error on server",
     });
   }
 };
 
+const uploadImage = async function (photo) {
+  try {
+    cloudinary.config({
+      cloud_name: "df9mh6l4n",
+      api_key: "368677466729715",
+      api_secret: "aZElKVwuvGJdPPZkOXAb-BRUk10",
+    });
 
+    const upload = await cloudinary.uploader.upload(photo.tempFilePath, {
+      public_id: new Date().toISOString(),
+    });
 
-
+    return upload.secure_url;
+  } catch (error) {
+    throw new Error("Failed to upload image to Cloudinary");
+  }
+};
 
 const editRecipes = async (req, res) => {
   try {
     const {
       params: { id },
-      body: { recipePicture, title, ingredients, video_link }
-    } = req
+      body: { recipePicture, title, ingredients, video_link },
+    } = req;
 
     if (isNaN(id)) {
       res.status(400).json({
         status: false,
-        message: 'ID must be integer'
-      })
+        message: "ID must be integer",
+      });
 
-      return
+      return;
     }
 
-    const checkData = await model.setGetRecipesByid(id)
+    const checkData = await model.setGetRecipesByid(id);
 
     // validasi jika id yang kita mau edit tidak ada di database
     if (!checkData.length) {
       res.status(404).json({
         status: false,
-        message: 'ID not found'
-      })
+        message: "ID not found",
+      });
 
-      return
+      return;
     }
 
     const payload = {
       recipePicture: recipePicture ?? checkData[0].recipePicture,
       title: title ?? checkData[0].title,
       ingredients: ingredients ?? checkData[0].ingredients,
-      video_link: video_link ?? checkData[0].video_link
-    }
+      video_link: video_link ?? checkData[0].video_link,
+    };
 
-    const query = await model.setEditRecipes(payload, id)
+    const query = await model.setEditRecipes(payload, id);
 
     res.send({
       status: true,
-      message: 'Success edit data',
-      data: query
-    })
+      message: "Success edit data",
+      data: query,
+    });
   } catch (error) {
     failed(res, {
       code: 500,
       payload: error.message,
-      message: 'Internal Server Error'
-    })
+      message: "Internal Server Error",
+    });
   }
-}
+};
 
 const editPhoto = async function (req, res) {
   try {
     const {
-      params: { id }
-    } = req
+      params: { id },
+    } = req;
 
-
-    const checkData = await model.setGetRecipesByid(id)
+    const checkData = await model.setGetRecipesByid(id);
 
     // validasi jika id yang kita mau edit tidak ada di database
     if (!checkData.length) {
       res.status(404).json({
         status: false,
-        message: 'ID not found'
-      })
+        message: "ID not found",
+      });
 
-      return
+      return;
     }
-    
 
     const { photo } = req?.files ?? {};
-
 
     if (!photo) {
       res.status(400).send({
@@ -302,7 +396,6 @@ const editPhoto = async function (req, res) {
         message: "Photo is required",
       });
     }
-
 
     let mimeType = photo.mimetype.split("/")[1];
     let allowFile = ["jpeg", "jpg", "png", "webp"];
@@ -315,38 +408,31 @@ const editPhoto = async function (req, res) {
       });
     }
 
-
-    
-      // validate size image
-      if (photo.size > 2000000) {
-        res.status(400).send({
-          status: false,
-          message: "File to big, max size 2MB",
-        });
-      }
-
-
-
-      cloudinary.config({
-        cloud_name: "df9mh6l4n",
-        api_key: "368677466729715",
-        api_secret: "aZElKVwuvGJdPPZkOXAb-BRUk10",
+    // validate size image
+    if (photo.size > 2000000) {
+      res.status(400).send({
+        status: false,
+        message: "File to big, max size 2MB",
       });
+    }
 
-      const upload = cloudinary.uploader.upload(photo.tempFilePath, {
-        public_id: new Date().toISOString(),
-      });
+    cloudinary.config({
+      cloud_name: "df9mh6l4n",
+      api_key: "368677466729715",
+      api_secret: "aZElKVwuvGJdPPZkOXAb-BRUk10",
+    });
 
-     
+    const upload = cloudinary.uploader.upload(photo.tempFilePath, {
+      public_id: new Date().toISOString(),
+    });
 
-      upload
+    upload
       .then(async (data) => {
         const payload = {
           recipePicture: data?.secure_url,
         };
-       
-        model.SeteditPhotoUser(payload, id);
 
+        model.SeteditPhotoUser(payload, id);
 
         res.status(200).send({
           status: false,
@@ -355,21 +441,11 @@ const editPhoto = async function (req, res) {
         });
       })
       .catch((err) => {
-        
         res.status(400).send({
           status: false,
           message: err,
         });
       });
-   
-
-
-
-
-
-
-
-
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -377,50 +453,49 @@ const editPhoto = async function (req, res) {
       message: "Error on server",
     });
   }
-}
-
+};
 
 const deleteRecipes = async (req, res) => {
   try {
     const {
-      params: { id }
-    } = req
+      params: { id },
+    } = req;
 
     if (isNaN(id)) {
       res.status(400).json({
         status: false,
-        message: 'ID must be integer'
-      })
+        message: "ID must be integer",
+      });
 
-      return
+      return;
     }
 
-    const checkData = await model.setGetRecipesByid(id)
+    const checkData = await model.setGetRecipesByid(id);
 
     // validasi jika id yang kita mau edit tidak ada di database
     if (!checkData.length) {
       res.status(404).json({
         status: false,
-        message: 'ID not found'
-      })
+        message: "ID not found",
+      });
 
-      return
+      return;
     }
 
-    const query = await model.setDeleteRecipes(id)
+    const query = await model.setDeleteRecipes(id);
     res.send({
       status: true,
-      message: 'Success delete data',
-      data: query
-    })
+      message: "Success delete data",
+      data: query,
+    });
   } catch (error) {
     failed(res, {
       code: 500,
       payload: error.message,
-      message: 'Internal Server Error'
-    })
+      message: "Internal Server Error",
+    });
   }
-}
+};
 
 module.exports = {
   getAllRecipes,
@@ -429,5 +504,7 @@ module.exports = {
   editRecipes,
   deleteRecipes,
   editPhoto,
-  getProfileRecipes
-}
+  getProfileRecipes,
+  uploadImage,
+  getRecipesPopular,
+};
--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -10,6 +10,9 @@ router.get('/recipes/:id', controller.getRecipesById)
 // get all data
 router.get('/recipes', controller.getAllRecipes)
 
+// get data popular
+router.get('/popular', controller.getRecipesPopular)
+
 
 // get recipes data profile
 router.get('/recipes/profile/me',middleware, controller.getProfileRecipes)
@@ -18,6 +21,10 @@ router.get('/recipes/profile/me',middleware, controller.getProfileRecipes)
 
 // insert data
 router.post('/recipes', middleware, controller.addNewRecipes)
+
+// add photo recipes data
+router.post("/recipes/photo",  controller.uploadImage);
+
 
 // edit data
 router.patch('/recipes/:id', controller.editRecipes)
